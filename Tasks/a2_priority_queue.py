9e0dfb08b8eb110bbc9de389d76f45ef142ffd8d
elevoro <sparklers@rambler.ru>
2021-05-27 23:35:41 +0300

    решение a2_test_priority_queue с помощью defaultdict

--- Tasks/a2_priority_queue.py
+++ Tasks/a2_priority_queue.py
@@ -4,6 +4,9 @@ Priority Queue
 Queue priorities are from 0 to 10
 """
 from typing import Any
+from collections import defaultdict
+
+d = defaultdict(list)
 
 
 def enqueue(elem: Any, priority: int = 0) -> None:
@@ -13,7 +16,7 @@ def enqueue(elem: Any, priority: int = 0) -> None:
     :param elem: element to be added
     :return: Nothing
     """
-    return None
+    d[priority].append(elem)
 
 
 def dequeue() -> Any:
@@ -22,7 +25,11 @@ def dequeue() -> Any:
 
     :return: dequeued element
     """
-    return None
+    sorted_tuple = sorted(d.items(), key=lambda x: x[0])
+    dict_ = dict(sorted_tuple)
+    for priority in dict_:
+        if len(dict_[priority]) > 0:
+            return dict_[priority].pop(0)
 
 
 def peek(ind: int = 0, priority: int = 0) -> Any:
@@ -32,7 +39,8 @@ def peek(ind: int = 0, priority: int = 0) -> Any:
     :param ind: index of element (count from the beginning)
     :return: peeked element
     """
-    return None
+    if len(d[priority]) > ind:
+        return d[priority][ind]
 
 
 def clear() -> None:
@@ -41,4 +49,7 @@ def clear() -> None:
 
     :return: None
     """
-    return None
+    for priority in d:
+        d[priority].clear()
+
+
