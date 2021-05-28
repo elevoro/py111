e650367fb7bee298f46a7060d6a6338ec9ecc015
elevoro <sparklers@rambler.ru>
2021-05-28 17:12:45 +0300

    решение a2_test_check_brackets

--- Tasks/a3_check_brackets.py
+++ Tasks/a3_check_brackets.py
@@ -5,5 +5,18 @@ def check_brackets(brackets_row: str) -> bool:
     :param brackets_row: input string to be checked
     :return: True if valid, False otherwise
     """
+    stack = []
+    for symb in brackets_row:
+        if symb == '(':
+            stack.append(")")
+        elif symb == ')':
+            if len(stack) > 0:
+                stack.pop()
+            else:
+                return False
+    if len(stack) == 0:
+        return True
+    else:
+        return False
 
-    return False
+
