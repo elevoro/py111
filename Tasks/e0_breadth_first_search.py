341474c14309d8335e8b0164f42677df6540bb69
elevoro <sparklers@rambler.ru>
2021-06-26 21:35:10 +0300

    e0 breadth search

--- Tasks/e0_breadth_first_search.py
+++ Tasks/e0_breadth_first_search.py
@@ -10,5 +10,14 @@ def bfs(g: nx.Graph, start_node: Hashable) -> List[Hashable]:
     :param start_node: starting node for search
     :return: list of nodes in the visited order
     """
-    print(g, start_node)
-    return list(g.nodes)
+    already_seen = {start_node}
+    queue = [start_node]
+    sorted_node = []
+    while queue:
+        node = queue.pop(0)
+        sorted_node.append(node)
+        for neighbor in g.neighbors(node):
+            if neighbor not in already_seen:
+                already_seen.add(neighbor)
+                queue.append(neighbor)
+    return sorted_node
