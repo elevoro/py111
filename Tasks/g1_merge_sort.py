8f570f47d38e4e8e6238fef6a9e9428913038ab3
elevoro <sparklers@rambler.ru>
2021-07-08 21:07:58 +0300

    merge sort

--- Tasks/g1_merge_sort.py
+++ Tasks/g1_merge_sort.py
@@ -8,4 +8,28 @@ def sort(container: List[int]) -> List[int]:
     :param container: container of elements to be sorted
     :return: container sorted in ascending order
     """
-    return container
+    if len(container) < 2:
+        return container
+    else:
+        mid = len(container) // 2
+        left, right = sort(container[:mid]), sort(container[mid:])
+        return merge(left, right)
+
+
+def merge(left, right):
+    result = []
+    i, j = 0, 0
+    while i < len(left) and j < len(right):
+        if left[i] <= right[j]:
+            result.append(left[i])
+            i += 1
+        else:
+            result.append(right[j])
+            j += 1
+    while i < len(left):
+        result.append(left[i])
+        i += 1
+    while j < len(right):
+        result.append(right[j])
+        j += 1
+    return result
