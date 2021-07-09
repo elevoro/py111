eee28b34ab2e9884281f57a8fc9dcb027f01c9fa
elevoro <sparklers@rambler.ru>
2021-07-09 20:26:22 +0300

    kmp

--- Tasks/h0_KMP.py
+++ Tasks/h0_KMP.py
@@ -8,8 +8,15 @@ def _prefix_fun(prefix_str: str) -> List[int]:
     :param prefix_str: dubstring for prefix function
     :return: prefix values table
     """
-    print(prefix_str)
-    return []
+    prefix_list = [0]*len(prefix_str)
+    for i in range(1, len(prefix_str)):
+        j = prefix_list[i-1]
+        while j > 0 and prefix_str[j] != prefix_str[i]:
+            j = prefix_list[j-1]
+        if prefix_str[j] == prefix_str[i]:
+            j = j + 1
+        prefix_list[i] = j
+    return prefix_list
 
 
 def kmp_algo(inp_string: str, substr: str) -> Optional[int]:
@@ -20,6 +27,15 @@ def kmp_algo(inp_string: str, substr: str) -> Optional[int]:
     :param substr: substr to be found in inp_string (needle)
     :return: index where first occurrence of substr in inp_string started or None if not found
     """
-
-    print(inp_string, substr, _prefix_fun)
-    return None
+    ind = None
+    prefix_list = _prefix_fun(substr)
+    j = 0
+    for i in range(len(inp_string)):
+        while j > 0 and substr[j] != inp_string[i]:
+            j = prefix_list[j-1]
+        if substr[j] == inp_string[i]:
+            j = j + 1
+        if j == len(substr):
+            ind = i - len(substr) + 1
+            break
+    return ind
