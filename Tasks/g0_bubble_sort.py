48c06175a5cdf6bc2054387e4650a76054fafa3b
elevoro <sparklers@rambler.ru>
2021-07-09 21:37:07 +0300

    bubble true/false

--- Tasks/g0_bubble_sort.py
+++ Tasks/g0_bubble_sort.py
@@ -10,8 +10,12 @@ def sort(container: List[int]) -> List[int]:
     """
     n = 1
     while n < len(container):
+        bubble = False
         for i in range(len(container) - n):
             if container[i] > container[i + 1]:
                 container[i], container[i + 1] = container[i + 1], container[i]
+                bubble = True
         n += 1
+        if not bubble:
+            break
     return container
